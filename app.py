fa32b2e6723df36a9c321767a158d7e2e5b834c9
husisy <zchaozhang@outlook.com>
2023-07-28 12:37:08 +0800

    add trubrics

--- app.py
+++ app.py
@@ -3,23 +3,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import streamlit as st
+from trubrics.integrations.streamlit import FeedbackCollector
 
 st.title('just for fun')
 
-
-def hf_set_seed():
+def is_valid_int_str(x):
     try:
-        seed = int(st.session_state['input_seed'])
+        int(x)
+        ret = True
     except:
-        seed = int(np.random.default_rng().integers(int(1e18)))
-    st.session_state['seed'] = str(seed)
-    st.session_state['np_rng'] = np.random.default_rng(seed)
-tmp0 = 'Set random seed (input empty to randomize)'
-tmp1 = st.session_state.get('seed', None)
-if tmp1 is None:
-    tmp1 = str(np.random.default_rng().integers(int(1e18)))
-    st.session_state['np_rng'] = np.random.default_rng(int(tmp1))
-st.text_input(tmp0, tmp1, key='input_seed', on_change=hf_set_seed)
+        ret = False
+    return ret
+
+
+tmp0 = '**Random seed** (input empty to randomize)'
+seed = st.session_state.get('seed', None)
+if (seed is None) or (not is_valid_int_str(seed)):
+    seed = int(np.random.default_rng().integers(int(1e18)))
+else:
+    seed = int(seed)
+np_rng = np.random.default_rng(seed)
+st.text_input(tmp0, str(seed), key='seed')
 
 
 # https://fonts.google.com/noto/specimen/Noto+Sans+SC
@@ -27,7 +31,6 @@ font_path = pathlib.Path('NotoSansSC-Regular.otf')
 
 def hf_draw_figure(text):
     text = [x.strip() for x in text.split(',')]
-    np_rng = st.session_state['np_rng']
     fig,ax = plt.subplots()
     for text_i in text:
         pos_x,pos_y = np_rng.uniform(0, 1, size=(2))
@@ -40,5 +43,19 @@ def hf_draw_figure(text):
 
 default_value = 'Doge, 鼠鼠我呀, 瘫, 屯屯鼠本鼠, Kokomi'
 st.text_input('keywords', default_value, key='text_input')
-tmp0 = st.session_state.get('text_input', default_value)
-hf_draw_figure(tmp0)
+text_input = st.session_state.get('text_input', default_value)
+hf_draw_figure(text_input)
+
+
+collector = FeedbackCollector(
+    component_name="demo-streamlit",
+    email=st.secrets.trubrics.email, # Store your Trubrics credentials in st.secrets:
+    password=st.secrets.trubrics.password, # https://blog.streamlit.io/secrets-in-sharing-apps/
+)
+
+collector.st_feedback(
+    feedback_type="thumbs",
+    model='none',
+    metadata=dict(seed=seed, text=text_input),
+    open_feedback_label="[Optional] Provide additional feedback",
+)
